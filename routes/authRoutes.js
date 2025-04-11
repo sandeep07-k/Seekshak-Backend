faa406801d2c988c2de0c88359d5e694dfd2e4ff
sandeep kumar <122988674+sandeep07-k@users.noreply.github.com>
2025-04-12 00:07:58 +0530

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { signup, login, resetPassword } = require("./controllers/authController");
+const { signup, login, resetPassword } = require("../controllers/authcontroller");
 
 router.post("/signup", signup);
 router.post("/login", login);
