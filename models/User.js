887bdc152521d40fb72fdd58969ca47163e5b69b
Sandeep <sandipkumar9334@gamil.com>
2025-04-14 16:35:43 +0530

    Updated something

--- models/User.js
+++ models/User.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema({
   userId: { type: String, unique: true },
   name: {type:String , sparse: true},
-  role: { type: String, enum: ["Student", "Teacher", "Institute"], required: true },
+  role: { type: String, enum: ["Student", "Tutor", "Institute"], required: true },
   // gender: {String},
   email: { type: String, unique: true, sparse: true },
   phone: { type: String, unique: true, required: true },
