4c507af44997c38cf56ea8eb91fe44d95e2681e8
Sandeep <sandipkumar9334@gamil.com>
2025-04-14 16:52:30 +0530

    Updated user.js

--- models/User.js
+++ models/User.js
@@ -4,8 +4,11 @@ const userSchema = new mongoose.Schema({
   userId: { type: String, unique: true },
   name: {type:String , sparse: true},
   role: { type: String, enum: ["Student", "Tutor", "Institute"], required: true },
-  // gender: {String},
-  email: { type: String, sparse: true },
+  email: {
+    type: String,
+    unique: true,
+    sparse: true,
+  },
   phone: { type: String, unique: true, required: true },
   password: { type: String, required: true },
   firebaseUid: {
