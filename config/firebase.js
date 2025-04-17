3484d0ea0f2678a49056c949a671164a28aafb71
Sandeep <sandipkumar9334@gamil.com>
2025-04-17 17:52:33 +0530

    Updated .gitignore

--- config/firebase.js
+++
@@ -1,8 +0,0 @@
-const admin = require("firebase-admin");
-const serviceAccount = require("./seekshak-c0835-firebase-adminsdk-fbsvc-2df7d74887.json");
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-});
-
-module.exports = admin;
