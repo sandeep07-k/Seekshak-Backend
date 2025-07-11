406b2b877bcced1ef958e3e1519dc0139891793f
Sandeep <sandipkumar9334@gamil.com>
2025-07-11 20:34:53 +0530

    added posts , postroutes

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const admin = require("../config/firebase");
-const Post = require("../models/Posts");
+const Post = require("../models/Post");
 
 router.post("/", async (req, res) => {
   try {
