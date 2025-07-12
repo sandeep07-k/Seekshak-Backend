40c08b20858a34af13fd9446279ab172322ac05e
Sandeep <sandipkumar9334@gamil.com>
2025-07-13 02:40:16 +0530

    updated user role auth controller

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const router = express.Router();
-const { signup, login,  checkUserExists } = require("../controllers/authcontroller");
+const { signup, login,  checkUserExists, getRoleByPhone } = require("../controllers/authcontroller");
 const verifyFirebaseToken = require("../middleware/verifyFirebaseToken");
 
 router.post("/signup", verifyFirebaseToken, signup);
 router.post("/login", verifyFirebaseToken, login);
 router.get('/check-user', checkUserExists);
-router.get('/get-role', authcontroller.getRoleByPhone);
+router.get('/get-role', getRoleByPhone);
 
 
 
