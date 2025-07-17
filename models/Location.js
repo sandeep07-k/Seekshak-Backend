6ad662abe03eaa33e3660d1cec26c004a1fe7b63
Sandeep <sandipkumar9334@gamil.com>
2025-07-17 11:51:32 +0530

    added location.js

--- models/Location.js
+++ models/Location.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const locationSchema = new mongoose.Schema({
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  userId: { type: String, required: true },
 
   location: {
     type: {
