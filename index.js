8bb5d80e67e8492845c40c23fce29850e4d385bf
Sandeep <sandipkumar9334@gamil.com>
2025-07-22 16:38:31 +0530

    added user.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
 const bodyParser = require("body-parser");
+const userRoutes = require("./routes/userRoutes");
 
 
 // Load env vars
